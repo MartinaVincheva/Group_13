2fa9ceccdbadac24c6f35e23b6fef5f048ce3b4b
Delyan Gavrailov <43825159+atomcraft@users.noreply.github.com>
2021-01-25 01:23:47 +0200

    Update project_Group15.c
    Input check & numberDigits (Georgi's version)

--- project_Group15.c
+++ project_Group15.c
@@ -1,58 +1,52 @@
 /* Напишете функция, която получавa  като параметър две числа, и връща като резултат броя на позициите в битовото представяне на числата, на които двете числа имат различни стойности. Водещите нули да се игнорират (тоест броенето да започне от най-старшия вдигнат бит на по-голямото число). */
 #include <stdio.h>
 
-unsigned int findDif(unsigned int a, unsigned int b);
-void decToBin(unsigned int n);
-
+int numberDigits(unsigned int, unsigned int);
+void decToBin(unsigned int);
 int main(void){
-
-    int a, b;
-
-    printf("\nEnter decimal value for A: ");
-    scanf("%d", &a);
-
-    if (a < 0){
-        a = ~a;
-        a += 1;
+	int a, b, status;
+    printf("Enter decimal value for A: ");
+    status = scanf("%d", &a);
+    if (status == 0){
+        printf("Invalid decimal number A.");
+        return 1;
+    }    
+    if(a < 0){
+       	a = ~a;
+       	a += 1;
     }
     printf("You entered A with absolute value of %d\n", a);
     decToBin(a);
-
-
     printf("\n\nEnter decimal value for B: ");
-    scanf("%d", &b);
-
+    status = scanf("%d", &b);
+    if (status == 0){
+        printf("Invalid decimal number A.");
+        return 1;
+    }    
     if (b < 0){
-        b = ~b;
-        b += 1;
+       	b = ~b;
+       	b += 1;
     }
     printf("You entered B with absolute value of %d\n", b);
     decToBin(b);
-
-    printf("\n\nThe sum of the differences between the numbers on bit level is: %d\n\n", findDif(a, b));
-
+    printf("\n\nThe sum of the differences between the numbers on bit level is: %d\n\n", numberDigits(a, b));
     return 0;
 }
-
-unsigned int findDif(unsigned int a,unsigned int b){
-    unsigned int res = a ^ b;
-    unsigned int counter = 0;
-    unsigned char cCheck = 1;
-
-    do{
-        counter += res & cCheck;
-    }while(res>>=1);
-
-    return counter;
+int numberDigits(unsigned int a, unsigned int b){
+    int c = a ^ b;
+	int n = 0;
+	while(c != 0){
+		if(c & 1)
+			++ n;
+		c >>= 1;
+   	}
+    return n;
 }
-
 void decToBin(unsigned int n){
-
-    printf("Binary value is: ");
-
-    unsigned i;
-    unsigned iCheck = 31;
-    for (i = 1 << iCheck; i > 0; i = i >> 1){
-        (n & i) ? printf("1") : printf("0");
+     printf("Binary value is: ");
+     unsigned i;
+     unsigned iCheck = 31;
+     for (i = 1 << iCheck; i > 0; i = i >> 1){
+       	(n & i) ? printf("1") : printf("0");
     }
 }
